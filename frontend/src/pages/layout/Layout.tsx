ff8f97251ed9fd068a67f6c44b21865d9e19cbae
Colby Ford <colbytylerford@gmail.com>
2024-08-09 12:33:31 -0400

    Add martini logo

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -4,7 +4,8 @@ import { Dialog, Stack, TextField } from '@fluentui/react'
 import { CopyRegular } from '@fluentui/react-icons'
 
 import { CosmosDBStatus } from '../../api'
-import Contoso from '../../assets/Contoso.svg'
+// import Contoso from '../../assets/Contoso.svg'
+import Contoso from '../../assets/Martini.svg'
 import { HistoryButton, ShareButton } from '../../components/common/Button'
 import { AppStateContext } from '../../state/AppProvider'
 
