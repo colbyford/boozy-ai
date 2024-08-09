aa71f0b90a7dc6e99fa2522271e01683a50d5edd
Colby Ford <colbytylerford@gmail.com>
2024-08-08 22:23:17 -0400

    Change Contoso to Boozy,ai

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Boozy.ai"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "Boozy.ai"
+    chat_description: str = "Looking for a good cocktail to shake up?"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
